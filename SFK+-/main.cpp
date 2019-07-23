78362f5f551b5422cc9ee1f4c1d365dd35802a35
sfktrkl <sfktrkl@gmail.com>
2019-07-24 00:57:22 +0300

    Create Lexer and Parser
    * Lexer and Parser functions are created.
    * YAZDIR is added.

---
+++ SFK+-/main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+const std::string OpenFile(const char* filename)
+{
+	std::ifstream file(filename);
+	std::string str((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+	return str;
+}
+
+const std::vector<std::string> Lexer(const std::string& fileContents)
+{
+	std::vector<std::string> tokens;
+	std::string token = "";
+	bool state = false;
+	std::string text = "";
+
+	for (char ch : fileContents)
+	{
+		token += ch;
+		if (token == " ")
+		{
+			if (state == false)
+				token = "";
+			else
+			{
+				text += ch;
+				token = "";
+			}
+		}
+		else if (token == "\n")
+			token = "";
+		else if (token == "YAZDIR")
+		{
+			tokens.push_back(token);
+			token = "";
+		}
+		else if (token == "\"")
+		{
+			if (state == false)
+				state = true;
+			else if (state == true)
+			{
+				tokens.push_back(text);
+				text = "";
+				state = false;
+				token = "";
+			}
+		}
+		else if (state == true)
+		{
+			text += ch;
+			token = "";
+		}
+	}
+
+	//for (std::string tok : tokens)
+	//	std::cout << tok << std::endl;
+
+	return tokens;
+}
+
+void Parser(std::vector<std::string> tokens)
+{
+	//for (std::string tok : tokens)
+	//	std::cout << tok << std::endl;
+	int i = 0;
+	while (i < tokens.size())
+	{
+		if (tokens[i] == "YAZDIR")
+		{
+			std::cout << tokens[i + 1] << std::endl;
+			i += 2;
+		}
+	}
+}
+
+int main(int argc, const char *argv[])
+{
+	std::string fileContents;
+
+	if (argc == 1) 
+	{
+		std::cout << "No Arguments passed!!" << std::endl;
+		std::cout << "Give A File Name" << std::endl;
+		std::string fileName;
+		std::getline(std::cin, fileName);
+		fileContents = OpenFile(fileName.c_str());
+	}
+	else 
+	{
+		std::cout << "Arguments: ";
+		for (int i = 1; i < argc; ++i) {
+			std::cout << argv[i] << std::endl;
+			fileContents = OpenFile(argv[i]);
+		}
+	}
+
+	std::vector<std::string> tokens = Lexer(fileContents);
+	Parser(tokens);
+
+	std::getline(std::cin, fileContents);
+
+	return 0;
+}
